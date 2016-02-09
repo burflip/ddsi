601f91bb68271c8a8a1176865ba5333b3acdbccf
Pablo Lara <pablo95lp@gmail.com>
2016-02-10 00:10:49 +0100

    Falta asociación con otros módulos

--- resources/views/proyectos/create.blade.php
+++ resources/views/proyectos/create.blade.php
@@ -1,73 +1,22 @@
-@extends('index')
+@extends("create")
 
-<style>
-    textarea {
-        resize: none;
-    }
-</style>
-
-@section('title')
-    Crear proyecto
+@section("title")
+    Crear un nuevo proyecto
 @endsection
 
-@section('elem_title')
-    Nuevo proyecto
+@section("resource_title")
+    Crear un nuevo proyecto
 @endsection
 
-@section('elem_description')
-    Introduce los datos del proyecto a crear.
-@endsection
-
-@section('search')
-    @include('_search', ['search_route' => 'proyecto.show', 'searchbox_text' => 'Buscar un proyecto...'])
-@endsection
-
-@section('form')
-    {!! Form::open(['method' => 'post', 'route' => 'proyecto.store']) !!}
-        <!-- User ID input -->
-        <div class="form-group">
-            {!! Form::label('user_id','Id del usuario:') !!}
-            {!! Form::number('user_id',null,['class' => 'form-control', "min" => '0']) !!}
-        </div>
-
-        <!-- Project's name input -->
-        <div class="form-group">
-            {!! Form::label('name','Nombre del proyecto:') !!}
-            {!! Form::text('name',null,['class' => 'form-control']) !!}
-        </div>
-
-        <!-- Image's URL input -->
-        <div class="form-group">
-            {!! Form::label('img_url','URL de la imagen asociada:') !!}
-            {!! Form::text('img_url',null,['class' => 'form-control']) !!}
-        </div>
-
-        <!-- Total amount input -->
-        <div class="form-group">
-            {!! Form::label('total_amount','Importe total del proyecto:') !!}
-            {!! Form::number('total_amount',null,['class' => 'form-control',"min" => '0' , "step" => 'any']) !!}
-        </div>
-
-        <!-- Starting date input -->
-        <div class="form-group">
-            {!! Form::label('starting_date','Fecha de inicio del proyecto:') !!}
-            {!! Form::input('date','starting_date',date('Y-m-d'),['class' => 'form-control']) !!}
-        </div>
-
-        <!-- Ending date input -->
-        <div class="form-group">
-            {!! Form::label('ending_date','Fecha de finalización del proyecto:') !!}
-            {!! Form::input('date','ending_date',date('Y-m-d'),['class' => 'form-control']) !!}
-        </div>
-
-        <!-- Notes input -->
-        <div class="form-group">
-            {!! Form::label('notes','Notas:') !!}
-            {!! Form::textarea('notes',null,['class' => 'form-control']) !!}
-        </div>
-        <p>
-        <!-- Submit bottom -->
-        <button type="submit" class="btn btn-default">Crear proyecto</button>
-        </p>
+@section("form")
+    {!! Form::open(["method" => "post", "route" => "proyecto.store"]) !!}
+    @include("proyectos._model")
     {!! Form::close() !!}
+@endsection
+
+@section("scripts")
+    @parent
+    <script>
+        $('.datepicker').pickadate();
+    </script>
 @endsection
