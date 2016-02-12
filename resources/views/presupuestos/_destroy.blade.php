6f7e8b0a83e8a29b00735236ec74b05790c08ec2
Pablo Lara <pablo95lp@gmail.com>
2016-02-12 14:06:07 +0100

    Bugfix

--- resources/views/presupuestos/_destroy.blade.php
+++ resources/views/presupuestos/_destroy.blade.php
@@ -3,7 +3,7 @@
         <h3>Eliminar presupuesto #{{ $presupuesto->id }}</h3>
     </div>
     <div class="col s12">
-        {!! Form::open(["method" => "delete", "route" => ["producto.destroy", $presupuesto->id]]) !!}
+        {!! Form::open(["method" => "delete", "route" => ["presupuesto.destroy", $presupuesto->id]]) !!}
         {!! Form::button("Eliminar", ["type" => "submit", "class" => "btn waves-effect waves-light left red"]) !!}
         {!! Form::close() !!}
     </div>
