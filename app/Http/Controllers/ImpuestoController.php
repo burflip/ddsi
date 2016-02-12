1e3ae877f42a1bbe269e97be4f8e02f313ef93a6
Pablo Lara <pablo95lp@gmail.com>
2016-02-12 14:02:21 +0100

    Falta función search
    Y probablemente mucho más

--- app/Http/Controllers/ImpuestoController.php
+++ app/Http/Controllers/ImpuestoController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Impuesto;
+use Auth;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -16,7 +19,8 @@ class ImpuestoController extends Controller
      */
     public function index()
     {
-        //
+        $impuestos=Impuesto::orderBy('created_at', 'desc')->paginate(15);
+        return view('impuestos.index',compact('impuestos'));
     }
 
     /**
@@ -26,7 +30,7 @@ class ImpuestoController extends Controller
      */
     public function create()
     {
-        //
+        return view('impuestos.create');
     }
 
     /**
@@ -37,7 +41,35 @@ class ImpuestoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try{
+            $impuesto = new Impuesto();
+            $impuesto->user_id = Auth::id();
+            $this->silentSave($impuesto,$request);
+        } catch (ModelNotFoundException $e) {
+            session()->flash('flash_message', 'Ha habido un error');
+        }
+
+        session()->flash('flash_message', 'Se ha creado el impuesto #'.$impuesto->id.' - '.$impuesto->name.' con éxito');
+        return redirect()->route("impuesto.index");
+    }
+
+    /**
+     * Basic save operation used for update & store.
+     *
+     * @param $impuesto
+     * @param Request $request
+     * @param bool $save
+     * @return mixed
+     */
+    public function silentSave(&$impuesto, Request $request,$save = true)
+    {
+        $impuesto->last_update_user_id=Auth::id();
+        $impuesto->name=$request->input('name');
+        $impuesto->aplicable_to_all=$request->input('aplicable_to_all');
+        $impuesto->fixed_amount=$request->input('fixed_amount');
+        $impuesto->percentage=$request->input('percentage');
+        ($save) ? $impuesto->save() : null;
+        return $impuesto;
     }
 
     /**
@@ -48,7 +80,8 @@ class ImpuestoController extends Controller
      */
     public function show($id)
     {
-        //
+        $impuesto=Impuesto::findOrFail($id);
+        return view('impuestos.show',compact('impuesto'));
     }
 
     /**
@@ -59,7 +92,8 @@ class ImpuestoController extends Controller
      */
     public function edit($id)
     {
-        //
+        $impuesto=Impuesto::findOrFail($id);
+        return view('impuestos.edit',compact('impuesto'));
     }
 
     /**
@@ -71,7 +105,15 @@ class ImpuestoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try{
+            $impuesto = Impuesto::findOrFail($id);
+            $this->silentSave($impuesto,$request);
+        } catch (ModelNotFoundException $e) {
+            session()->flash('flash_message', 'Ha habido un error');
+        }
+
+        session()->flash('flash_message', 'Se ha actualizado el impuesto #'.$impuesto->id.' - '.$impuesto->name.' con éxito');
+        return redirect()->route("dashboard");
     }
 
     /**
@@ -82,6 +124,15 @@ class ImpuestoController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $impuesto = Impuesto::findOrFail($id);
+        $impuesto->delete();
+        session()->flash('flash_message', 'Se ha eliminado el impuesto #'.$id.' con éxito');
+        return redirect()->route("impuesto.index");
+    }
+
+    public function search($name)
+    {
+        $impuestos = Impuesto::where("name",$name)->orderBy('created_at', 'desc')->paginate(10);
+        return view("impuestos.index",compact("impuestos"));
     }
 }
